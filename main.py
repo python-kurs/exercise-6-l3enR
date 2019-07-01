ea4d539c9f36c0fae94597fb656d68ee6faa00e9
BenR <44396287+BenR32@users.noreply.github.com>
2019-07-01 19:29:08 +0200

    ass 6

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 # Exercise 6
 from pathlib import Path
+from satpy import Scene, find_files_and_readers
+import satpy as satpy
+
 
 input_dir  = Path("data")
+output_dir = Path("output")
 
 # 1. Read the Scene that you downloaded from the data directory using SatPy. [2P]
+files = find_files_and_readers(base_dir = input_dir, reader = "seviri_l1b_nc")
+scn = Scene(filenames = files)
 
 # 2. Load the composites "natural_color" and "convection" [2P]
+scn.load(["natural_color"])
+scn.load(["convection"])
 
 # 3. Resample the fulldisk to the Dem. Rep. Kongo and its neighbours [4P] 
 #    by defining your own area in Lambert Azimuthal Equal Area. 
@@ -13,6 +21,25 @@ input_dir  = Path("data")
 #      - lat and lon of origin: -3/23
 #      - width and height of the resulting domain: 500px
 #      - projection x/y coordinates of lower left: -15E5
-#      - projection x/y coordinates of upper right: 15E5 
+#      - projection x/y coordinates of upper right: 15E5
+ 
+from utils import resample_area
+local_scn = resample_area(area_id = "Democratic_Republic_Kongo", 
+                          description = "Democratic Republic Kongo and its neighbors in Lambert Azimuthal Equal Area projection",
+                          proj_id = "Democratic_Republic_Kongo",
+                          proj_dict = {"proj":"laea", "lat_0": -3, "lon_0":23},
+                          width = 500,
+                          height = 500,
+                          llx = -15E5,
+                          lly = -15E5,
+                          urx = 15E5,
+                          ury = 15E5,
+                          scn = scn)
 
 # 4. Save both loaded composites of the resampled Scene as simple png images. [2P]
+#construct output pathes
+naturalCol = output_dir / "natural_color.png"
+convect = output_dir / "convection.png"
+
+local_scn.show("natural_color").save(naturalCol)
+local_scn.show("convection").save(convect)
--- utils.py
+++ utils.py
@@ -2,3 +2,48 @@
 
 # Use this file for all functions you create and might want to reuse later.
 # Import them in the `main.py` script
+
+def resample_area(area_id, description, proj_id, proj_dict, width, height, llx, lly, urx, ury, scn):
+    """
+    Resample the used projection to the defined parameters and returns a local scene.
+    
+    The resample parameters are setted and are used in the AreaDefinition function. After that a local scene is generated.
+    
+    Parameters
+    ----------
+    area_id     : String
+                  Unique name of the local scene
+    description : String
+                  Describe the scene
+    proj_id     : String
+                  Describe the projection
+    proj_dict   : Dictionary
+                  A dictionary with the keys "proj" for projection, "lat_0" and "lon_0" for the latitude and longitude origin
+    width       : number
+                  The widht of the bounding box
+    height      : number
+                  The height of the bounding box
+    llx         : number
+                  Lower left x corner
+    lly         : number
+                  Lower left y corner
+    urx         : number
+                  Upper right x corner
+    ury         : number
+                  Upper right y corner
+        
+    Returns
+    -------
+    scene object
+    
+    """
+    from pyresample.geometry import AreaDefinition
+    
+    #bounding box
+    area_extent = (llx, lly, urx, ury)
+    
+    #resample the area
+    area_def = AreaDefinition(area_id, proj_id, description, proj_dict, width, height, area_extent)
+    local_scn = scn.resample(area_def)
+    
+    return local_scn
